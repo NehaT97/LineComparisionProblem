01be6bc2f1816c04b3d2cdb06e8baa28dbc89e62
unknown <nehathakur764@gmail.com>
2020-11-20 09:38:49 +0530

    Check Equality of two lines Based On End Points

--- src/com/LineComparision/LineComparision.java
+++ src/com/LineComparision/LineComparision.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class LineComparision
 {
+
     /* Read the Coordinates*/
          private static Line readLineCoordinates()
          {
@@ -22,23 +23,43 @@ public class LineComparision
                 return new Line(x1, x2, y1, y2);
          }
 
+
     /* UC1 : Calculate length of the line using cartesian system */
-    public static double calculateLengthByCartesianSystem(final double x1, final double x2, final double y1, final double y2) {
-        double xCoordinatePower = Math.pow((x2 - x1), 2);
-        double yCoordinatePower = Math.pow((y2 - y1), 2);
-        return Math.sqrt(xCoordinatePower + yCoordinatePower);
-    }
+        public static double calculateLengthByCartesianSystem(final double x1, final double x2, final double y1, final double y2)
+        {
+                double xCoordinatePower = Math.pow((x2 - x1), 2);
+                double yCoordinatePower = Math.pow((y2 - y1), 2);
+                return Math.sqrt(xCoordinatePower + yCoordinatePower);
+        }
 
 
-    public static void main(String args[])
-    {
-        System.out.println("Welcome to Line Comparison Computation Program");
-        System.out.println("Enter the Line1 co-ordinates");
-        Line line1 = readLineCoordinates();
-        System.out.println("Length of the line is : " + calculateLengthByCartesianSystem(line1.getX1(), line1.getX2(), line1.getY1(), line1.getY2()));
+    /* UC2 : isEqualTo method for lines using java equalTo*/
+         public static Boolean isEqualTo(Line line1, Line line2)
+         {
+                System.out.println("Line1 \t(x1, y1): ( " + line1.getX1() + ", " + line1.getY1() + ")" + "\t(x2, y2): ( " + line1.getX2() +", " +line1.getY2() +")");
+                System.out.println("Line2 \t(x1, y1): ( " + line2.getX1() + ", " + line2.getY1() + ")" + "\t(x2, y2): ( " + line2.getX2() +", " +line2.getY2() +")");
+                Double lengthOfLine1 = calculateLengthByCartesianSystem(line1.getX1(), line1.getX2(), line1.getY1(), line1.getY2());
+                Double lengthOfLine2 = calculateLengthByCartesianSystem(line2.getX1(), line2.getX2(), line2.getY1(), line2.getY2());
+                boolean isEqual = lengthOfLine1.equals(lengthOfLine2);
+                if (isEqual) {
+                    System.out.println("Line1 and Line2 both are equals");
+                } else {
+                    System.out.println("Line1 and Line2 both are not equal");
+                }
+                return isEqual;
+         }
 
-        System.out.println("\nEnter the Line2 co-ordinates");
-        Line line2 = readLineCoordinates();
-        System.out.println("Length of the line is : " + calculateLengthByCartesianSystem(line2.getX1(), line2.getX2(), line2.getY1(), line2.getY2()));
-    }
+        public static void main(String args[])
+         {
+                System.out.println("Welcome to Line Comparison Computation Program");
+                System.out.println("Enter the Line1 co-ordinates");
+                Line line1 = readLineCoordinates();
+                System.out.println("Length of the line is : " + calculateLengthByCartesianSystem(line1.getX1(), line1.getX2(), line1.getY1(), line1.getY2()));
+
+                System.out.println("\nEnter the Line2 co-ordinates");
+                Line line2 = readLineCoordinates();
+                System.out.println("Length of the line is : " + calculateLengthByCartesianSystem(line2.getX1(), line2.getX2(), line2.getY1(), line2.getY2()));
+
+                isEqualTo(line1, line2);
+         }
 }
